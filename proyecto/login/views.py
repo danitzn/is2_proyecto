d71425e31ab6af2f44c8920d3183a758b3d1e4e8
andresarevca <andres.arevalo@definetec.com.py>
2023-03-04 16:38:59 -0300

    Add login views and styles.

--- proyecto/login/views.py
+++ proyecto/login/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import FormView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
+class LoginView(FormView):
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+
+    def form_valid(self, form):
+        # Authenticate the user
+        user = form.get_user()
+        login(self.request, user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return '/admin'
